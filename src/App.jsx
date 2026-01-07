aba8ba16ae041426984a0cc2a52a094f08e987b4
Punitgithub22 <punitfullstackdeveloper22@gmail.com>
2026-01-08 01:08:20 +0530

    add files to git

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  //  
+
+  return (
+    <>
+      <h1>This is Punit DevOps Solutions </h1>
+      <p>We Provide Digital Solutions</p>
+
+    </>
+  )
+}
+
+export default App
